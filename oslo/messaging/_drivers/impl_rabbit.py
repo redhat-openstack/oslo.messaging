57cf9fce530d1f1fe077d33721ca17169428f103
Dan Smith <dasmith@redhat.com>
2015-01-08 11:41:07 -0500

    Fix typo in reconnect exception handler
    Resolves: rhbz#1175685
    
    Signed-off-by: Lon Hohberger <lhh@redhat.com>

--- oslo/messaging/_drivers/impl_rabbit.py
+++ oslo/messaging/_drivers/impl_rabbit.py
@@ -615,7 +615,7 @@ class Connection(object):
                 # a protocol response.  (See paste link in LP888621)
                 # So, we check all exceptions for 'timeout' in them
                 # and try to reconnect in this case.
-                if 'timeout' not in six.text_type(e):
+                if 'timeout' not in six.text_type(ex):
                     raise
                 e = ex
 
